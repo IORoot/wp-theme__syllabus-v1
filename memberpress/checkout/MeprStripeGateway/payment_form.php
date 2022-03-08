76794f1fd7baf1d341b05318e86fc97f30e178ba
Andy Pearson <andy@londonparkour.com>
2022-03-08 11:01:20 +0000

    stripe starting

--- memberpress/checkout/MeprStripeGateway/payment_form.php
+++ memberpress/checkout/MeprStripeGateway/payment_form.php
@@ -6,6 +6,13 @@
   }
 ?>
 
+<?php
+// ┌─────────────────────────────────────────────────────────────────────────┐
+// │                                                                         │
+// │                            STRIPE WALLET?                               │
+// │                                                                         │
+// └─────────────────────────────────────────────────────────────────────────┘
+?>
 <?php if($payment_method->settings->stripe_wallet_enabled == 'on' && $payment_method->settings->stripe_checkout_enabled != 'on') { ?>
   <div class="mepr-stripe-payment-request-wrapper">
     <div id="mepr-stripe-payment-request-element" style="  max-width: 300px" class="mepr-stripe-payment-request-element" data-stripe-public-key="<?php echo esc_attr($payment_method->settings->public_key); ?>" data-payment-method-id="<?php echo esc_attr($payment_method->settings->id); ?>" data-locale-code="<?php echo $mepr_options->language_code; ?>" data-currency-code="<?php echo $mepr_options->currency_code; ?>" data-total-text="<?php echo esc_attr(__('Total', 'memberpress')); ?>">
@@ -14,23 +21,53 @@
     <br>
   </div>
 <?php } ?>
+
+
+<?php
+// ┌─────────────────────────────────────────────────────────────────────────┐
+// │                                                                         │
+// │                              STRIPE FORM                                │
+// │                                                                         │
+// └─────────────────────────────────────────────────────────────────────────┘
+?>
 <?php if($payment_method->settings->stripe_checkout_enabled == 'on'): ?>
   <?php MeprHooks::do_action('mepr-stripe-payment-form-before-name-field', $txn); ?>
   <input type="hidden" name="mepr_stripe_is_checkout" value="1"/>
   <input type="hidden" name="mepr_stripe_checkout_page_mode" value="1"/>
   <h4><?php _e('Pay with your Credit Card via Stripe Checkout', 'memberpress'); ?></h4>
   <span role="alert" class="mepr-stripe-checkout-errors"></span>
+
+<?php
+// ┌─────────────────────────────────────────────────────────────────────────┐
+// │                                                                         │
+// │                             NAME ON CARD                                │
+// │                       [STRIPE CHECKOUT NOT WORKING]                     │
+// │                                                                         │
+// └─────────────────────────────────────────────────────────────────────────┘
+?>
 <?php else: ?>
   <?php MeprHooks::do_action('mepr-stripe-payment-form-before-name-field', $txn); ?>
   <div class="mp-form-row">
-    <div class="mp-form-label">
+    <div class="mp-form-label flex flex-col">
       <label><?php _e('Name on the card:*', 'memberpress'); ?></label>
-      <span class="cc-error"><?php _ex('Name on the card is required.', 'ui', 'memberpress'); ?></span>
+      <span class="cc-error text-red-500 ml-auto"><?php _ex('Name on the card is required.', 'ui', 'memberpress'); ?></span>
     </div>
-    <input type="text" name="card-name" class="mepr-form-input stripe-card-name" required />
+    <input type="text" name="card-name" class="mepr-form-input stripe-card-name mepr-form-input w-full p-4 text-xl text-zinc-800 bg-amber-200 rounded" required />
   </div>
 
+
+  <?php
+  // ┌─────────────────────────────────────────────────────────────────────────┐
+  // │                                ACTION                                   │
+  // └─────────────────────────────────────────────────────────────────────────┘
+  ?>
   <?php MeprHooks::do_action('mepr-stripe-payment-form-card-field', $txn); ?>
+
+  <?php
+  // ┌─────────────────────────────────────────────────────────────────────────┐
+  // │                           STRIPE CARD                                   │
+  // └─────────────────────────────────────────────────────────────────────────┘
+  ?>
   <div class="mp-form-row">
     <div class="mp-form-label">
       <label><?php _e('Credit Card:*', 'memberpress'); ?></label>
@@ -41,5 +78,17 @@
     </div>
   </div>
 <?php endif; ?>
+
+<?php
+// ┌─────────────────────────────────────────────────────────────────────────┐
+// │                                ACTION                                   │
+// └─────────────────────────────────────────────────────────────────────────┘
+?>
 <?php MeprHooks::do_action('mepr-stripe-payment-form', $txn); ?>
+
+<?php
+// ┌─────────────────────────────────────────────────────────────────────────┐
+// │                                NO JS                                    │
+// └─────────────────────────────────────────────────────────────────────────┘
+?>
 <noscript><p class="mepr_nojs"><?php _e('Javascript is disabled in your browser. You will not be able to complete your purchase until you either enable JavaScript in your browser, or switch to a browser that supports it.', 'memberpress'); ?></p></noscript>
